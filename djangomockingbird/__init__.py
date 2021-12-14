3877193a12dfe318398f6871e1a83c08a4a7a259
larsvonschaff <larsvonschaff@gmail.com>
2021-12-15 00:22:13 +0100

    change init imports

---
+++ djangomockingbird/__init__.py
@@ -0,0 +1,7 @@
+from . import make_mocks
+from . import queryset
+from . import queryset_utils
+from . import utils
+from . import decorator
+from . import decorator_utils
+from .decorator import make_mocks
