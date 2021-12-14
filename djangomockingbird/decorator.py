79cac160ba4f0e66a4804ebd13bcca255110f6ed
larsvonschaff <larsvonschaff@gmail.com>
2021-12-15 00:31:31 +0100

    fix imports in decorator.py

--- djangomockingbird/decorator.py
+++ djangomockingbird/decorator.py
@@ -1,20 +1,20 @@
 from unittest.mock import patch
-from djangomockingbird.make_mocks import make_mocks
+from djangomockingbird import make_mocks
 from os import path
-from djangomockingbird.decorator_utils import resolve_input_path, build_file_path
+from djangomockingbird import decorator_utils
 
 
 def mock_model(string_path, specs):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            module, model_name = resolve_input_path(string_path)
-            final_path = build_file_path(module)
+            module, model_name = decorator_utils.resolve_input_path(string_path)
+            final_path = decorator_utils.build_file_path(module)
             if not path.exists(final_path):
                 raise Exception('Invalid input path. Please specify a correct path to the model you would like to mock!')
             importlib = __import__('importlib')
             blogposts = importlib.import_module(module)
             blog_post = getattr(blogposts, model_name)
-            mock_model = make_mocks(blog_post, specs=specs)
+            mock_model = make_mocks.make_mocks(blog_post, specs=specs)
             setattr(blogposts, model_name, mock_model)
             result = func(*args, **kwargs)
 
