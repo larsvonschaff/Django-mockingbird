22f38ce857725931c0c45ad7489f9c005ad48271
larsvonschaff <larsvonschaff@gmail.com>
2021-12-15 01:04:17 +0100

    change import form

--- djangomockingbird/decorator.py
+++ djangomockingbird/decorator.py
@@ -1,20 +1,21 @@
 from unittest.mock import patch
-from djangomockingbird import make_mocks
+from .make_mocks import make_mocks
 from os import path
-from djangomockingbird import decorator_utils
+from .decorator_utils import resolve_input_path
+from .decorator_utils import build_file_path
 
 
 def mock_model(string_path, specs):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            module, model_name = decorator_utils.resolve_input_path(string_path)
-            final_path = decorator_utils.build_file_path(module)
+            module, model_name = resolve_input_path(string_path)
+            final_path = build_file_path(module)
             if not path.exists(final_path):
                 raise Exception('Invalid input path. Please specify a correct path to the model you would like to mock!')
             importlib = __import__('importlib')
             blogposts = importlib.import_module(module)
             blog_post = getattr(blogposts, model_name)
-            mock_model = make_mocks.make_mocks(blog_post, specs=specs)
+            mock_model = make_mocks(blog_post, specs=specs)
             setattr(blogposts, model_name, mock_model)
             result = func(*args, **kwargs)
 
